3b150bd7a6d59fc2e49fef25f2b80af0fd4394e4
Hoa Luu <luukimhoa@gmail.com>
2018-09-11 10:12:57 -0500

    Add simple Css template and Template Menu

---
+++ src/ui/MenuController.java
@@ -0,0 +1,34 @@
+package ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.text.Text;
+
+public class MenuController {
+	@FXML
+	MenuBar mBar;
+	
+	@FXML
+	Menu admin;
+	@FXML
+	MenuItem books;
+	@FXML
+	MenuItem members;
+	
+	@FXML
+	Menu librian;
+	@FXML
+	MenuItem checkOutBook;
+	@FXML
+	MenuItem checkOutRecord;
+	
+	@FXML
+	Text welcomeText;
+	
+	
+	
+	
+
+}
