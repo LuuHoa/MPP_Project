03c98895703d2c2d69b273068d59c116d5bd2015
owner <owner@172.18.7.165>
2018-09-12 18:28:35 -0500

    add Navigation

--- src/business/Utilites.java
+++ src/business/Utilites.java
@@ -25,4 +25,22 @@ public class Utilites {
 		 return USER_LIST_DATA;
 		
 	}
+	
+	public static List<MemberList> getTableList_(){
+		
+
+		 List<LibraryMember> list = DataAccessService.getMemberLists();
+		 
+		 List<MemberList> tableList = new ArrayList<>();
+		 
+		 for(LibraryMember member :list) {
+			 MemberList m = new MemberList (member.getId() + "" , member.getFirstName(), member.getLastName(),
+					 member.getPhoneNumber(), member.getAddress().getStreet(), member.getAddress().getCity(),
+					 member.getAddress().getState(), member.getAddress().getZip()); 
+			 tableList.add(m);
+		 }
+		 ObservableList<MemberList> USER_LIST_DATA = FXCollections.observableArrayList(tableList);
+		 return tableList;
+		
+	}
 }
--- src/ui/AddNewBookController.java
+++ src/ui/AddNewBookController.java
@@ -7,14 +7,19 @@ import business.Address;
 import business.Author;
 import business.Book;
 import business.LibraryMember;
+import business.Utilites;
 import dataaccess.storge.DataAccessService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class AddNewBookController {
 	
@@ -49,15 +54,23 @@ public class AddNewBookController {
 	
 	@FXML
 	public Label error_txt;
+	int duration = 21;
 	
+	@FXML
+	public MenuItem ff;
+	@FXML
+	public void handleMenuButtonAction(ActionEvent event) {
+		String d = ((MenuItem)event.getSource()).getText().toString();
+		duration = Integer.parseInt(d);
+
+		System.out.println(d);
+	}
 	@FXML
 	public void handleSaveButtonAction(ActionEvent event) {
 		
 		String tilelStr = title.getText().toString();
 		String isbnStr = isbn.getText().toString();
-		String checkOutLen = checkout.getText().toString();
-		
-		System.out.println(checkOutLen);
+	
 		
 		String firstName = first_name.getText().toString();
 		String lastName = last_name.getText().toString();
@@ -69,7 +82,7 @@ public class AddNewBookController {
 		String telphoneStr = telphone.getText().toString();
 		String bioStr = bio.getText().toString();
 		
-		AddNewBookScreen addBookScreen = new AddNewBookScreen();
+
 		
 		
 		if(!firstName.equals("") && !last_name.equals("")&&  !city.equals("")
@@ -80,12 +93,20 @@ public class AddNewBookController {
 			Address address = new Address(streetStr,cityStr,stateStr,  zipStr );
 			List<Author> authorList = new ArrayList<>();
 			authorList.add(new Author(firstName,lastName ,address,telphoneStr, bioStr ));
-			Book book = new Book(isbnStr, 21, tilelStr, authorList);
+			Book book = new Book(isbnStr, duration, tilelStr, authorList);
 			
 			error_txt.setText("");
 			DataAccessService.addNewBook(book);
 			System.out.println("finished handleSaveButtonAction()");
-			addBookScreen.showBookListScreen();
+			
+			Node source = (Node) event.getSource();
+			Stage theStage = (Stage)source.getScene().getWindow();
+			
+			AdminScreen adminScreen = AdminScreen.INSTANCE;
+	    	adminScreen.setStage(theStage);
+			adminScreen.setData(Utilites.getTableList());
+	        adminScreen.show(); 
+	        theStage.hide();
 			
 		}else {
 		
