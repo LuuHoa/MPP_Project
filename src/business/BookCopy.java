558ea6c7369c38998646a7a49fd4ecada164e184
Hoa Luu <luukimhoa@gmail.com>
2018-09-12 10:33:30 -0500

    update

--- src/business/BookCopy.java
+++ src/business/BookCopy.java
@@ -6,7 +6,7 @@ public class BookCopy implements Serializable {
 	private static final long serialVersionUID = 8309080721495266420L;
 	private String copyNum;
 	private boolean isAvailable;
-<<<<<<< HEAD
+
 	private Book orgBook;
 
 	public BookCopy(Book orgBook, String copyNum) {
@@ -21,35 +21,22 @@ public class BookCopy implements Serializable {
 
 	public void setAvailable(boolean isAvailable) {
 		this.isAvailable = isAvailable;
-=======
-	Book book;
+	}
 
 	public BookCopy(String copyNum) {
 		this.copyNum = copyNum;
->>>>>>> cf3190d02321355def86e5521fb0aafb33e142a2
 	}
 
 	public String getCopyNum() {
 		return copyNum;
 	}
-<<<<<<< HEAD
 
 	public Book getOrgBook() {
 		return orgBook;
 	}
-	
-=======
->>>>>>> cf3190d02321355def86e5521fb0aafb33e142a2
 
 	public Boolean getIsAvailable() {
 		return isAvailable;
 	}
 
-	public BookCopy(String copyNum, Book book) {
-		this.copyNum = copyNum;
-		this.isAvailable = true;
-		this.book = book;
-	}
-	
-	
 }
