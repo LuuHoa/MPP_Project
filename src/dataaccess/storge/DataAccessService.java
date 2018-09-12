31903f0b3a53d68cf2129a41ac24b6b184dbb396
Hoa Luu <luukimhoa@gmail.com>
2018-09-12 13:18:52 -0500

    resolve merge code

--- src/dataaccess/storge/DataAccessService.java
+++ src/dataaccess/storge/DataAccessService.java
@@ -86,7 +86,7 @@ public class DataAccessService {
 	
 
 	public static void simulateData() {
-<<<<<<< HEAD
+
 		LibraryStaff s = new LibraryStaff("11", "123", "Admin");
 
 		LibraryStaff s2 = new LibraryStaff("22", "123", "Librarian");
@@ -124,57 +124,9 @@ public class DataAccessService {
 		
 		System.out.println(lm2);
 		WriteObjectToFile(bookFilepath, lm2);
-		// End
-=======
-
-		   LibraryStaff s = new LibraryStaff("11", "123", "Admin");
-		   
-		   LibraryStaff s2 = new LibraryStaff("22", "123", "Librarian");
-		   LibraryStaff s3 = new LibraryStaff("33", "123", "Both");
-		   
-		   List<Object> objectList = new ArrayList<>();
-		   objectList.add(s);
-		   objectList.add(s2);
-		   objectList.add(s3);
-		   WriteObjectToFile(userFilepath, objectList);
-
-//			Address address1 = new Address("1 st", "Fairfield", "Iowa", "234");
-//			LibraryMember member1 = new LibraryMember("0", "Eman", "Mohammed", address1, "123456789");
-//			Address address2 = new Address("2 st", "Fairfield", "Iowa", "234");
-//			LibraryMember member2 = new LibraryMember("1", "Thi Quynh", "Mai Tran", address2, "123456789");
-//			Address address3 = new Address("3 st", "Fairfield", "Iowa", "234");
-//			LibraryMember member3 = new LibraryMember("2", "Thi Kim Hoa ", "Luu", address3, "123456789");
-//			Address address4 = new Address("4 st", "Fairfield", "Iowa", "234");
-//			LibraryMember member4 = new LibraryMember("0", "Ali", "Mohammed", address4, "123456789");
-//
-//			List<Object> objectList2 = new ArrayList<>();
-//			objectList2.add(member1);
-//			objectList2.add(member2);
-//			objectList2.add(member3);
-//			objectList2.add(member4);
-//			WriteObjectToFile(memberFilepath, objectList2);
-			
-			// To be removed
-			List<Object> lm = new ArrayList<>();
-			lm.add(new LibraryMember("0", "Hai", "Orsi", new Address("620 Gonzales Drive", "Long Beach", "NY", "11561"), "123456789"));
-			lm.add(new LibraryMember("1", "Hai", "Orsi", new Address("9268 Arcadia Dr", "Lakewood", "NJ", "08701"), "123456789"));
-			lm.add(new LibraryMember("2", "Hai", "Orsi", new Address("7807 Lees Creek Street", "Upper Marlboro", "MD", "20772"), "123456789"));
-			lm.add(new LibraryMember("3", "Hai", "Orsi", new Address("8157 Heather Drive", "Tampa", "FL", "33604"), "123456789"));
-			lm.add(new LibraryMember("4", "Hai", "Orsi", new Address("75 Deerfield Rd", "Boca Raton", "FL", "33428"), "123456789"));
-			System.out.println(lm);
-			WriteObjectToFile(memberFilepath, lm);
-			
-			List<Object> lm2 = new ArrayList<>();
-			lm2.add(new Book("0061964361", 21, "Book 1", null));
-			lm2.add(new Book("0061964362", 7, "Book 2", null));
-			lm2.add(new Book("0061964363", 21, "Book 3", null,2));
-			lm2.add(new Book("0061964364", 7, "Book 4", null,2));
-			System.out.println(lm2);
-			WriteObjectToFile(bookFilepath, lm2);
-			// End		   
 
 
->>>>>>> remotes/origin/master
+
 	}
 
 	private static void WriteObjectToFile(String filepath, List<Object> serObj) {
